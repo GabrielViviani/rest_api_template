53bc53280c8c1f8ee330702578243a36c90c902b
Gabriel <gabriel.viviani2009@gmail.com>
2023-03-08 10:55:44 -0300

    first commit

---
+++ Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace REST_API_TEMPLATE.Models
+
+    // Essa classe define como um livro deve ser dentro do sistema
+{
+    public class Book
+    {
+        [Key]
+        public Guid Id { get; set; } 
+        public string? Title { get; set; }
+        public string? Subtitle { get; set; }
+        public string? Description { get; set; }
+        public Genre? Genre { get; set; }
+        public string? Publisher { get; set; }
+        public string? ISBN { get; set; }
+        public double? Rating { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public Guid? AuthorId { get; set; }
+        public Author? Author { get; set; }
+
+    }
+}
